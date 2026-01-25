d45717eca125440d0544ca5a58f7341d002366a1
김주영 <juyoungk09@gmail.com>
2026-01-25 13:02:15 +0900

    feat/confirm modal added

--- src/components/ui/modal/ConfirmModal.tsx
+++ src/components/ui/modal/ConfirmModal.tsx
@@ -4,15 +4,16 @@ import { Modal } from "./Modal";
 
 export default function ConfirmModal({ title, message, onClose, onConfirm }: { title?: string, message?: string, onClose: () => void, onConfirm: () => void }) {
     return (
-        <Modal className="max-w-md rounded-[20px]" isOpen={true} onClose={onClose} >
-            <div className="w-full h-full flex flex-col px-6 py-5 gap-6 items-center justify-center">
-                <div className="flex flex-col gap-1">
-                    <h2 className="font-semibold text-xl text-[#000000]">{title || "정말 전달을 완료하셨나요?"}</h2>
-                    <p className="text-[#505050] text-sm">{message || "'예'를 누르실 경우 더이상 분실물을 관리할 수 없게 됩니다. 이 작업은 되돌릴 수 없습니다."}</p>
+        <Modal className="max-w-md rounded-[20px] h-[230px] flex items-center justify-center" isOpen={true} onClose={onClose} >
+            <div className="max-w-[290px] flex flex-col items-center justify-center gap-[25px]">
+                <div className="flex flex-col items-center justify-center gap-[13px]">
+                    <h2 className="font-semibold text-2xl text-[#000000]">{title || "상태를 변경하시겠습니까?"}</h2>
+                    <p className="text-[#000000] text-xs text-center">{message || "상태를 변경하실 경우 변경된 상태는 #번 충전기 상세페이지에 기록 되며 해당 기록은 지우거나 수정할 수 없습니다."}</p>
                 </div>
-                <div className="w-full flex justify-end gap-3 text-base">
-                    <button onClick={() => onClose?.()} className="px-[22px] py-[10px] border-gray border bg-[#F9F9F9] text-gray-700 rounded-xl">아니요</button>
-                    <button onClick={() => onConfirm?.()} className="px-[22px] py-[10px] bg-[#000000] text-white rounded-xl">예</button>
+                <div className="w-full flex justify-center gap-12 text-base">
+                    <button onClick={() => onConfirm?.()} className="px-[22px] py-[10px] bg-[#0D0D0D] text-white rounded-[100px]">예</button>
+                    <button onClick={() => onClose?.()} className="px-[22px] py-[10px] border-[#0D0D0D] border bg-[#F9F9F9] text-[#0D0D0D] rounded-[100px]">아니요</button>
+                    
                 </div>
             </div>
         </Modal>
